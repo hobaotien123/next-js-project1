63e0b7aa2466a908ea6a363bc3793e6620c522db
nghiluc.nguyen <nghiluc.nguyen@citynow.vn>
2020-11-03 13:44:24 +0700

    update

--- pages/newPage/test1/[index].jsx
+++ pages/newPage/test1/[index].jsx
@@ -79,17 +79,17 @@ function newPage({ posts }) {
 export async function getStaticProps(next) {
     // Call an external API endpoint to get posts.
     // You can use any data fetching library
-    const res = await fetch('http://localhost:8082/posts');
-    console.log("res",res);
-    const posts = await res.json();
+    // const res = await fetch('http://localhost:8082/posts');
+    // console.log("res",res);
+    // const posts = await res.json();
   
-    // By returning { props: posts }, the Blog component
-    // will receive `posts` as a prop at build time
-    return {
-      props: {
-        posts,
-      },
-    }
+    // // By returning { props: posts }, the Blog component
+    // // will receive `posts` as a prop at build time
+    // return {
+    //   props: {
+    //     posts,
+    //   },
+    // }
 }
 export async function getStaticPaths() {
   return {
